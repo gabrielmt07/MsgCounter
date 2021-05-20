6fc678193c08652d0a8db32b0031dc36a998556a
unknown <gabriel_motta07@yahoo.com.br>
2021-05-20 18:40:23 -0300

    conexão com o bd e contagem das mensagens

---
+++ CountMsgs/Controllers/AcessarBd.cs
@@ -0,0 +1,46 @@
+﻿using CountMsgs.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Reflection;
+
+namespace CountMsgs.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcessarBd : ControllerBase
+    {
+        private readonly MainContext _db;
+
+        public AcessarBd(MainContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public string Get(int segundos)
+        {
+            using (var command = _db.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = $"SELECT COUNT(*) as contador FROM Envio WHERE Data >= DATEADD(SECOND, {-segundos}, GETDATE())";
+                _db.Database.OpenConnection();
+                using (var result = command.ExecuteReader())
+                {
+                    if(result.Read())
+                    {
+                        string msgCounter = result["contador"].ToString();
+                        return msgCounter;
+                    }
+                    return "Tabela vazia";
+                }
+            }
+            
+        }
+    }
+}
---
+++ CountMsgs/Data/MainContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CountMsgs.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Envio> Envios { get; set; }
+    }
+}
